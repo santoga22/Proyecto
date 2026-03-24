858031728c3d5d6287099ecb3a310f16fc56dee0
yeiko <yeiko@Yeikol>
2026-03-24 01:13:24 -0600

    Implementación de lista de atención e historial de pacientes
    - Se agregó la ListaAtencion como lista enlazada simple (FIFO)
    - Se agregó la ListaHistorial para almacenar pacientes atendidos
    - Se modificó el flujo de atención:
      - Pacientes se desencolan por prioridad (alta, media, baja)
      - Se agregan a la lista de atención
      - Al finalizar, se mueven al historial
    - Se agrego la opcion de Finalizar Atencion al menu (dentro de Emergencias)
    
    COLAS (prioridad)
            ↓
    LISTA EN ATENCION
            ↓
    LISTA HISTORIAL

---
+++ src/proyecto/pkgfinal/estructura/de/datos/ListaHistorial.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkgfinal.estructura.de.datos;
+
+/**
+ *
+ * @author yeiko
+ */
+public class ListaHistorial {
+    
+    private NodoPaciente inicio,fin;
+    
+    public boolean esVacia(){
+        return inicio == null;
+    }
+    
+    public void insertar(Paciente p){
+        NodoPaciente nuevo = new NodoPaciente(p);
+        
+        if (esVacia()){
+            inicio = fin = nuevo;
+        }else {
+            NodoPaciente aux = inicio;
+            while(aux.getSig() != null){
+                aux = aux.getSig();
+            }
+            aux.setSig(nuevo);
+        }
+        
+    }
+    
+    @Override
+    public String toString(){
+        String r = "";
+        NodoPaciente aux = inicio;
+        
+        while(aux != null){
+            r += aux + "\n";
+            aux = aux.getSig();
+        }
+        
+        return r;
+    }   
+       
+}
